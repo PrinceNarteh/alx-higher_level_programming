674fa2d367ea96aabc8e17db96fe27da89258403
Prince Narteh <prinarteh@gmail.com>
2023-04-11 21:07:05 -0700

    add  4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Contains the inherits_from function
+"""
+
+
+def inherits_from(obj, a_class):
+    """returns true if obj is a subclass of a_class, else false"""
+    return(issubclass(type(obj), a_class) and type(obj) != a_class)
